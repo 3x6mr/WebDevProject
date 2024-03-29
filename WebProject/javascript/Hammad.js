0065b621794641af3d6b39038e128fca333fb2dc
UchihaCoder <137582039+3x6mr@users.noreply.github.com>
2024-03-30 00:25:06 +0300

    Changes in database
    adding ID for each product

--- WebProject/javascript/Hammad.js
+++ WebProject/javascript/Hammad.js
@@ -1,25 +1,29 @@
 function searchItems() {
-    const searchInput = document.getElementById('searchInput').value.toLowerCase();
-    fetch('../javascript/db.json') 
-      .then(response => response.json())
-      .then(data => {
-        const filteredItems = data.products.filter(item => item.name.toLowerCase().includes(searchInput));
-        if (filteredItems.length === 0) {
-          displayNotFoundMessage();
-        } else {
-          displayItems(filteredItems);
-        }
-      })
-      .catch(error => console.error('Error fetching items:', error));
-  }
-  
-  function displayItems(items) {
-    const itemsContainer = document.getElementById('itemsContainer');
-    itemsContainer.innerHTML = '';
-    items.forEach(item => {
-      const itemElement = document.createElement('div');
-      itemElement.classList.add('item');
-      itemElement.innerHTML = `
+  const searchInput = document
+    .getElementById("searchInput")
+    .value.toLowerCase();
+  fetch("../javascript/db.json")
+    .then((response) => response.json())
+    .then((data) => {
+      const filteredItems = data.products.filter((item) =>
+        item.name.toLowerCase().includes(searchInput)
+      );
+      if (filteredItems.length === 0) {
+        displayNotFoundMessage();
+      } else {
+        displayItems(filteredItems);
+      }
+    })
+    .catch((error) => console.error("Error fetching items:", error));
+}
+
+function displayItems(items) {
+  const itemsContainer = document.getElementById("itemsContainer");
+  itemsContainer.innerHTML = "";
+  items.forEach((item) => {
+    const itemElement = document.createElement("div");
+    itemElement.classList.add("item");
+    itemElement.innerHTML = `
         <h2>${item.name}</h2>
         <p>Owner: ${item.owner}</p>
         <p>${item.description}</p>
@@ -28,15 +32,14 @@ function searchItems() {
         <p>Quantity: ${item.quantity}</p>
         <p>Section: ${item.section}</p>
       `;
-      itemsContainer.appendChild(itemElement);
-    });
-  }
-  
-  function displayNotFoundMessage() {
-    const itemsContainer = document.getElementById('itemsContainer');
-    itemsContainer.innerHTML = '<p>Bike not found</p>';
-  }
-  
-  // Initially display all items
-  searchItems();
-  
+    itemsContainer.appendChild(itemElement);
+  });
+}
+
+function displayNotFoundMessage() {
+  const itemsContainer = document.getElementById("itemsContainer");
+  itemsContainer.innerHTML = "<p>Bike not found</p>";
+}
+
+// Initially display all items
+searchItems();
